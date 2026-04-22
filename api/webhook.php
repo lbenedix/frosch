ce35b23196e993b869e129caf7e0e9e7a10df2b8
vwgb44b <lukas.benedix@volkswagen.de>
2026-04-22 11:35:18 +0200

    remove logging function

--- api/webhook.php
+++ api/webhook.php
@@ -1,13 +1,5 @@
 <?php
 
-$logFile = '/tmp/webhook.log';
-
-function logMessage(string $message): void {
-    global $logFile;
-    $timestamp = date('Y-m-d H:i:s');
-    file_put_contents($logFile, "[$timestamp] $message" . PHP_EOL, FILE_APPEND);
-}
-
 $secret = getenv('WEBHOOK_SECRET') ?: 'change-me';
 $providedToken = $_SERVER['HTTP_X_WEBHOOK_SECRET'] ?? $_GET['secret'] ?? '';
 
